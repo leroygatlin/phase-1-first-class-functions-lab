242f95dc365346b6b687b7e567b1c2bf5988c05c
leroygatlin <leroygatlin@gmail.com>
2023-11-07 14:24:58 -0800

    complete advanced first class function

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function(array1){
+    return array1.slice(0,2);
+ }
+
+ const returnLastTwoDrivers = function(array1){
+    return array1.slice(2,4);
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(multiplier){
+    return function(fare){
+       return fare * multiplier;
+    }
+}
+
+const fareDoubler = createFareMultiplier(2)
+
+const fareTripler = createFareMultiplier(3)
+
+function selectDifferentDrivers(array1,selectingDrivers){
+    return selectingDrivers(array1)
+    
+}
